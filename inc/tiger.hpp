066d8b0482fd203f88f2a26ad13b7a1c3056e6dc
Dmitry Ponyatov <dponyatov@gmail.com>
2026-01-03 15:37:03 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- inc/tiger.hpp
+++ inc/tiger.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
+/// @defgroup libc libc
+/// @{
+#include <iostream>
+/// @}
+
 /// @defgroup main main
 /// @{
-
 extern int main(int argc, char *argv[]);
 extern void arg(int argc, char *argv);
-
 /// @}
--- src/tiger.cpp
+++ src/tiger.cpp
@@ -1,6 +1,6 @@
 #include "tiger.hpp"
 
-int main(int argc, char *argv[]) {  //
+int main(int argc, char *argv[]) {
     arg(0, argv[0]);
     for (int i = 1; i < argc; i++) {  //
         arg(i, argv[i]);
